14e5b794491a80ac63410b718cb33ee9f6acfdc8
Fred Bradley <hello@fredbradley.co.uk>
2017-04-11 12:30:18 +0100

    Tidy readme file

--- load.php
+++ load.php
@@ -3,7 +3,7 @@
 Plugin Name: Upfront Mama Upfront Mama Loves
 Description: A Upfront Mama Loves Custom Must Have Item for Nicola
 Author: Fred Bradley
-Version: 1.3.0
+Version: 1.3.1
 Author URI: http://fred.im/
 */
 namespace UpfrontMama\Loves;
